e9379aa4d5756456bf51d33b443f15bc923889de
Ham Vocke <hermann.vocke@gmail.com>
2014-09-30 23:22:31 +0200

    added timestamp to post entry, return all entries on GET

--- selfapi/api.py
+++ selfapi/api.py
@@ -1,17 +1,25 @@
 from . import con, api
 from flask.ext.restful import reqparse, Resource
 from flask import jsonify, request
+import datetime
+import json
+from bson import json_util
+from bson.objectid import ObjectId
 
 parser = reqparse.RequestParser()
 parser.add_argument('diet', type=str)
 
+def toJson(data):
+    return json.dumps(data, default=json_util.default)
+
 class Diet(Resource):
     def get(self):
         collection = con['selfapi'].diet
-        entry = collection.DietEntry.find_one()
-        return jsonify(timestamp = entry.timestamp,
-                   title = entry.title,
-                   value = entry.value)
+        entries = collection.DietEntry.find()
+        json_entries = []
+        for entry in entries:
+            json_entries.append(entry)
+        return toJson(json_entries)
 
     def post(self):
         args = parser.parse_args()
@@ -19,6 +27,8 @@ class Diet(Resource):
         entry = collection.DietEntry()
         entry.title = request.form['title']
         entry.value = int(request.form['value'])
+        entry.created_at = datetime.datetime.now()
+        entry.timestamp = datetime.datetime.strptime(request.form['timestamp'], '%Y-%m-%d %H:%M')
         entry.save()
         return {'status': 'success'}, 202
 
