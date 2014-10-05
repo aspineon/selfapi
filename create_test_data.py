d406918086f21595dbcb3cfe96e18708daa919ca
Ham Vocke <hermann.vocke@gmail.com>
2014-10-05 20:30:21 +0200

    added dummy file for creating mongodb test data

---
+++ create_test_data.py
@@ -0,0 +1,8 @@
+from selfapi import db, con
+
+
+def clean():
+    pass
+
+def init():
+    pass
--- run.py
+++ run.py
@@ -1,2 +1,7 @@
 from selfapi import app
+import create_test_data
+
+create_test_data.clean()
+create_test_data.init()
+
 app.run()
