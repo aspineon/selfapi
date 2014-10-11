d7d17edc0c5178dc73dad3ad4b561561969cace7
Ham Vocke <hermann.vocke@gmail.com>
2014-10-11 11:48:54 +0200

    corrected data types for incoming diet post requests

--- selfapi/api.py
+++ selfapi/api.py
@@ -2,6 +2,7 @@ from . import db, api
 from flask.ext.restful import reqparse, Resource, abort, fields, marshal_with
 from datetime import datetime
 from bson.objectid import ObjectId
+from flask import request
 
 parser = reqparse.RequestParser()
 
@@ -28,20 +29,11 @@ class DietList(Resource):
             abort(404, message="No entries found")
         return wrap_as_list(entries)
 
-
-class Diet(Resource):
-    @marshal_with(diet_fields)
-    def get(self, entry_id):
-        collection = db.diet
-        entry = collection.DietEntry.find_one({"_id": ObjectId(entry_id)})
-        if not entry:
-            abort(404, message="Diet Entry {} doesn't exist".format(entry_id))
-        return entry
-
     def post(self):
-        parser.add_argument('title', type=str, required=True)
+        print(request)
+        parser.add_argument('title', type=unicode, required=True)
         parser.add_argument('value', type=int, required=True)
-        parser.add_argument('timestamp', type=str)
+        parser.add_argument('timestamp', type=unicode)
         args = parser.parse_args()
 
         collection = db.diet
@@ -54,7 +46,17 @@ class Diet(Resource):
             entry.timestamp = datetime.strptime(args['timestamp'], '%Y-%m-%d %H:%M')
         entry.save()
 
-        return {'status': 'created'}, 202
+        return {'status': 'created'}, 201
+
+
+class Diet(Resource):
+    @marshal_with(diet_fields)
+    def get(self, entry_id):
+        collection = db.diet
+        entry = collection.DietEntry.find_one({"_id": ObjectId(entry_id)})
+        if not entry:
+            abort(404, message="Diet Entry {} doesn't exist".format(entry_id))
+        return entry
 
 api.add_resource(DietList, '/api/diet')
 api.add_resource(Diet, '/api/diet/<string:entry_id>')
