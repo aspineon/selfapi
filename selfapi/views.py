a508d079e72b2314a2a7fd4bc57b6e60298e1bf0
Ham Vocke <hermann.vocke@gmail.com>
2014-11-13 22:20:28 +0100

    added view for today's entries

--- selfapi/views.py
+++ selfapi/views.py
@@ -1,6 +1,7 @@
 from flask import render_template, request
 from . import app, api, db
 from selfapi import DietEntry, Profile
+from datetime import date, timedelta
 
 @app.route('/')
 def index():
@@ -12,8 +13,14 @@ def diet_form():
     entries = list(DietEntry.query.all())
     return render_template('diet.html', entries = entries)
 
+@app.route('/diet/today', methods = ['GET'])
+def diet_form_today():
+    today = date.today()
+    tomorrow = date.today() + timedelta(days=1)
+    entries = list(DietEntry.query.filter(DietEntry.timestamp.between(today, tomorrow)))
+    return render_template('diet.html', entries = entries)
+
 @app.route('/profile', methods = ['GET'])
 def profile():
     profile = Profile.query.first()
-    print(profile)
     return render_template('profile.html', profile=profile)
