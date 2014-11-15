5ce38355119521b2fe68b52ecb04505cdf86d34d
Ham Vocke <hermann.vocke@gmail.com>
2014-11-15 12:37:09 +0100

    removed instance config for heroku deployment

--- selfapi/__init__.py
+++ selfapi/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 from flask.ext.restful import Api
 from flask.ext.sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, instance_relative_config=True)
+# app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
-app.config.from_pyfile('config.py')
+# app.config.from_pyfile('config.py')
 
 api = Api(app)
 
