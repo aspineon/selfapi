79d97d9bc900f00a11b0a9c9b4d4d86166fc8229
Ham Vocke <hermann.vocke@gmail.com>
2014-11-15 13:07:40 +0100

    changing default sqlite path to current app dir

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 DEBUG = False
 CSRF_ENABLED = True
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////var/go/selfapi.db'
+# SQLALCHEMY_DATABASE_URI = 'sqlite:////var/go/selfapi.db'
--- selfapi/__init__.py
+++ selfapi/__init__.py
@@ -4,8 +4,11 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 # TODO figure out how to do instance deployent for heroku
 app = Flask(__name__, instance_relative_config=True)
+
+SQLALCHEMY_DATABASE_URI = 'sqlite:////{0}/selfapi.db'.format(app.root_path)
+
 app.config.from_object('config')
-#app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', silent=True)
 
 api = Api(app)
 
